b58516080b5abecd8ad7920722c9d7ed2c4a7a5e
frag70 <167621871+frag70@users.noreply.github.com>
2026-08-26 15:17:36 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.ts
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
